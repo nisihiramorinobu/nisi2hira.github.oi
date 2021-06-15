6c713006dcc1a2e9d4c8d63017566e13ce2c3047
nisihiramorinobu <84651061+nisihiramorinobu@users.noreply.github.com>
2021-06-15 11:11:28 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+'use strict';
+
+console.clear();
+
+// sitetop jcpwm
+{
+
+  // function tesuto() {
+  //   document.getElementById('moreBtn').addEventListener('click', () => {
+  //     $("#textBox").css( { 'font-size' : '18px' , 'color' : 'red' });
+  //     const i = 0;
+  //     console.log(i); 
+  //   });
+
+  // }
+
+  // tesuto();
+  
+$(function(){
+  $("#moreBtn").click(function(){
+      var switch_flg = $(this).hasClass("close");
+      if (switch_flg) {
+          $("#moreBtn img").attr('src', '/re_common/images/sitetop/sp/icon_open.png');
+          $(this).removeClass("close");
+          $(".sp_jcpwm ul li:nth-child(n+2)").fadeOut(200);
+          $(".listbtn").fadeOut(200);
+      }else{
+          $("#moreBtn img").attr('src', '/re_common/images/sitetop/sp/icon_close.png');
+          $(this).addClass("close");
+          $(".sp_jcpwm ul li:nth-child(n+2)").css('display','none').fadeIn(500);
+          $(".listbtn").css('display','none').fadeIn(500);
+      }
+  });
+});
+
+}
