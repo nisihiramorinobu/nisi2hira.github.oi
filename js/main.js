a71b19a19627f8bafcc6f19c9a889c95f15747ad
nisihiramorinobu <84651061+nisihiramorinobu@users.noreply.github.com>
2021-07-03 18:11:58 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,37 @@
-'use strict'
+'use strict';
 
+console.clear();
+
+// sitetop jcpwm
 {
+
+  // function tesuto() {
+  //   document.getElementById('moreBtn').addEventListener('click', () => {
+  //     $("#textBox").css( { 'font-size' : '18px' , 'color' : 'red' });
+  //     const i = 0;
+  //     console.log(i); 
+  //   });
+
+  // }
+
+  // tesuto();
   
-  const h1 = document.querySelector('h1');
-  h1.textContent = window.innerWidth;
-  const p = document.querySelector('p');
-  p.textContent = window.innerHeight;
+$(function(){
+  $('header').load('more.html');
+  $("#moreBtn").click(function(){
+      var switch_flg = $(this).hasClass("close");
+      if (switch_flg) {
+          $("#moreBtn img").attr('src', '/re_common/images/sitetop/sp/icon_open.png');
+          $(this).removeClass("close");
+          $(".sp_jcpwm ul li:nth-child(n+2)").fadeOut(200);
+          $(".listbtn").fadeOut(200);
+      }else{
+          $("#moreBtn img").attr('src', '/re_common/images/sitetop/sp/icon_close.png');
+          $(this).addClass("close");
+          $(".sp_jcpwm ul li:nth-child(n+2)").css('display','none').fadeIn(500);
+          $(".listbtn").css('display','none').fadeIn(500);
+      }
+  });
+});
+
 }
