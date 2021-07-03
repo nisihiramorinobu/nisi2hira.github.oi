365b1eb5ea7caf9c655769999b7422435dae563b
nisihiramorinobu <84651061+nisihiramorinobu@users.noreply.github.com>
2021-07-03 18:37:16 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,37 +1,70 @@
-'use strict';
+'use strict'
 
-console.clear();
+$(function() {
 
-// sitetop jcpwm
-{
+const narrs = [1,2,3,4,5];
+const warrs = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+];
+narrs.forEach(narr => {
+  console.log(narr); 
+});
+warrs.forEach((warr,index) => {
+  console.log(`warr ${index}`);
+});
+function f_a(i) {
+  return i + 1000;
+}
 
-  // function tesuto() {
-  //   document.getElementById('moreBtn').addEventListener('click', () => {
-  //     $("#textBox").css( { 'font-size' : '18px' , 'color' : 'red' });
-  //     const i = 0;
-  //     console.log(i); 
-  //   });
+// document.querySelector('button').addEventListener('click', () => {
+   $('button').click(function() {
+    $('footer').load('more.html');
 
-  // }
+    // var parent = document.createElement("div");
+    // var child = document.createElement("p");
+    // parent.appendChild(child);
+    // var span = document.createElement("span");
+    
+    // child.after(span);
+    // $('section').append(parent);
+    // console.log(parent.outerHTML);
+    // "<div><p></p><span></span></div>"
+// document.getElementById ('target').textContent = target.dataset.eigo;
+  //  $('#target').text($('#target').data('eigo'));
 
-  // tesuto();
+// const li = document.createElement('li');
+// li.textContent = 'item2';
+  //  let li = $('<li>').text('item2');
+
+// document.querySelector('ul').appendChild(li);
+  //  $('ul').append(li);
   
-$(function(){
-  // $('header').load('more.html');
-  $("#moreBtn").click(function(){
-      var switch_flg = $(this).hasClass("close");
-      if (switch_flg) {
-          $("#moreBtn img").attr('src', '/re_common/images/sitetop/sp/icon_open.png');
-          $(this).removeClass("close");
-          $(".sp_jcpwm ul li:nth-child(n+2)").fadeOut(200);
-          $(".listbtn").fadeOut(200);
-      }else{
-          $("#moreBtn img").attr('src', '/re_common/images/sitetop/sp/icon_close.png');
-          $(this).addClass("close");
-          $(".sp_jcpwm ul li:nth-child(n+2)").css('display','none').fadeIn(500);
-          $(".listbtn").css('display','none').fadeIn(500);
-      }
-  });
+    const copy1 = document.querySelector('ul').cloneNode(true); //子要素込みで
+      //  const copy1 =  $('ul').clone(true);
+console.log(copy1);
+// jQuery はtrue,falseのがちがう
+
+  document.querySelector('section').insertBefore(copy1,document.querySelector('ul'));
+    // $('ul > li:eq(2)').before(copy1);  
+    // copy1.insertBefore($('ul > li:eq(2)'));
+
+  //  const copy2 = document.querySelectorAll('li')[0].cloneNode(false); 
+//       const copy2 =  $('ul > li:eq(0)').clone(false);
+// console.log(copy2);
+
+  //  document.querySelector('ul').insertBefore(copy2,items[1]);
+      // $('ul > li:eq(1)').before(copy2);  
+      // $('ul > li:eq(1)').after(copy2); 
+      // copy2.insertBefore($('ul > li:eq(1)')) ;
+      // copy2.insertAfter($('ul > li:eq(1)')) ;
+
 });
 
-}
+
+
+
+}); 
